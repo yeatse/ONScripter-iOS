ca7ea99ef751488645a41499c5d4a43621bdea8b
Yeatse CC <iyeatse@gmail.com>
2017-01-28 11:18:40 +0800

    fix bitcode configuration error

--- dev_iPhoneOS.sh
+++ dev_iPhoneOS.sh
@@ -1,6 +1,6 @@
 export set PATH=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin:$PATH
 export set SDK_CC=`xcrun -find -sdk iphoneos clang`
-export set SDK_CFLAGS='-arch armv7 -arch arm64 -miphoneos-version-min=8.0 -fembed-bitcode-marker'
+export set SDK_CFLAGS='-arch armv7 -arch arm64 -miphoneos-version-min=8.0 -fembed-bitcode'
 export set SDK_PATH=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 export set HOST=arm-apple-darwin
 export set INSTALL_PATH=$PWD/Release-iphoneos
